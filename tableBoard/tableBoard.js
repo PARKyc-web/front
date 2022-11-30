46cf6312bd53c6155320fb9d011975cfd0106313
PARKyc <erty1201@naver.com>
2022-11-30 15:42:46 +0900

    Json 파일 읽기 추가

---
+++ tableBoard/tableBoard.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){    
+    
+    $.getJSON("tableBoard.json", function(data){
+        console.log(data);
+
+        /** 
+         *  하나의 문자열을 만드는 방법
+         */
+        for(let i=0; i<data.length; i++){            
+            var row = "<tr>";
+            row +="<td>" + data[i].no + "</td>";
+            row +="<td class='table-title'><a href='#'>" + data[i].title + "</a></td>";
+            row +="<td>" + data[i].reg + "</td>";
+            row +="<td>" + data[i].view + "</td>";
+
+            if(data[i].file == 'Y'){
+                row +="<td><img src='paper-clip.png'></td>";
+            }
+            row += "</tr>";
+
+            $(".table-body").append(row);
+        }
+
+
+    });    
+});
+
